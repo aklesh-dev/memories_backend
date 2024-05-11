7a8c738b273f28bd451c919efc7a834885c79255
aklesh yadav <arzunayavadu998@gmail.com>
2024-05-11 09:07:26 +0545

    ch

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ import helmet  from 'helmet';
 dotenv.config();
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
+
+mongoose.connect(CONNECTION_URL)
+   .then(() =>  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+   .catch((error) => console.log(error.message));
+
+
+// Set X-XSS-Protection
+app.use(helmet.xssFilter());
+
+
 // Set Content Security Policy
 app.use(
    helmet.contentSecurityPolicy({
@@ -32,10 +44,4 @@ app.use('/posts', postRoutes);
 
 
 
-const PORT = process.env.PORT || 5000;
-const CONNECTION_URL = process.env.CONNECTION_URL;
-
-mongoose.connect(CONNECTION_URL)
-   .then(() =>  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-   .catch((error) => console.log(error.message));
 
